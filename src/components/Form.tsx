f8ea21760a42507635e47aeb632d72a2bd87e0a8
Alex Francescon <alex.francescon@hotmail.it>
2025-11-08 13:36:23 +0100

    clip trapezoid

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,6 @@
 export const Form = () => {
   return (
-    <div className="relative h-screen w-screen overflow-hidden bg-primary clip-trapezoid">
+    <div className="relative h-[120vh] w-screen overflow-hidden bg-primary clip-trapezoid-both">
       <div className="p-10 text-white">
         <h2 className="text-3xl font-bold">Contact Form</h2>
         {/* Your form elements go here */}
